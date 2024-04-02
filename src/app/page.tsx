e3f744bf1c980ad6b8a3c134196a324032c5743b
leeseonhee17 <leeseonhee17@gmail.com>
2024-04-02 16:10:59 +0900

    #3.4강의일부 가상요소 구분자

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@
      ex) hover:  객체에 마우스를 가져다 댔을 때 
          dark :  화면모드가 다크모드일때
          active: 마우스 클릭 시 
+         placeholder : 가상요소 
 
 */
 
@@ -11,36 +12,16 @@ export default function Home(){
   return (
     <main className="bg-gray-300 h-screen flex items-center justify-center p-5 dark:bg-gray-700">
           <div className="bg-white w-full shadow-lg p-5 rounded-2xl max-w-screen-sm dark:bg-gray-500 ">
-            <div className="flex justify-between items-center">
-              <div className="flex flex-col">
-                <span className="text-gray-600 font-semibold -mb-1 dark:text-gray-300">In transit</span>
-                <span className="text-4xl font-semibold">Coolblue</span>
-              </div>
-              <div className="size-12 bg-orange-400 rounded-full">
-                  <span>Coolblue</span>
-              </div>
-            </div>
-            
-            <div className="my-2 flex items-center gap-2">
-              <span className="bg-green-400 mr-2 text-white 
-              uppercase rounded-full px-2.5 py-1.5 text-xs 
-              font-medium transition hover:bg-green-500 hover:scale-125">Today</span>
-              <span className="dark:text-white">9:30-10:30</span>
-            </div>  
-            
-            <div className="relative">
-              <div className="bg-gray-200 absolute
-                                rounded-full w-full h-2" />
-              <div className="bg-green-400 absolute 
-                              w-2/3 h-2 rounded-full" />
-            </div>
-            
-            <div className="flex justify-between items-center mt-5 text-gray-600 dark:text-gray-500">
-              <span>Expected</span>
-              <span>Sorting center</span>
-              <span>In transit</span>
-              <span className="text-gray-400">Delivered</span>
-            </div>
+            <input className="w-full rounded-full h-10 bg-gray-200 pl-5 outline-none ring
+            ring-transparent focus:ring-orange-500 focus:ring-offset-2 transition-shadow
+            placeholder:drop-shadow"
+            type="text" placeholder="Search here..."
+            />
+            <button className="bg-black w-full text-white py-2
+            rounded-full active:scale-90 mt-2
+            transition-transform font-medium outline-none">
+            Search
+            </button>
           </div>
       </main>
     );
